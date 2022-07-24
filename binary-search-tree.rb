8acdfde55b110f3fae5038627d0f44239a26c93b
Red <crabmarket56@gmail.com>
2022-07-24 11:45:58 -0400

    Add sorting and remove duplicates

--- binary-search-tree.rb
+++ binary-search-tree.rb
@@ -11,12 +11,16 @@ end
 class Tree
 
   def initialize(array)
-    @root = build_tree(array)
-
+    sorted = bubble_sort(array.uniq)
+    @root = build_tree(sorted)
+    
   end
   
   def build_tree(array)
-
+    p first = array[0]
+    p last = array[-1]
+    p mid = array.length/2
+    
   end
 
   def insert(value)
@@ -63,4 +67,21 @@ class Tree
 
   end
 
+  def bubble_sort(array)
+    
+    for i in 0...array.length
+    
+      for b in 0...(array.length - i - 1) 
+       if array[b] > array[b + 1]
+         array[b], array[b + 1] = array[b + 1], array[b]
+       end
+      end
+    end
+    return array
+  end
+
 end
+
+array_data = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
+
+p data_tree = Tree.new(array_data)
