b990ce300ce877b059df41a278763dfa80cc52d3
Red <crabmarket56@gmail.com>
2022-07-24 10:51:11 -0400

    Create initial file

---
+++ binary-search-tree.rb
@@ -0,0 +1,55 @@
+class Node
+  include Comparable
+end
+
+class Tree
+
+  def build_tree
+
+  end
+
+  def insert
+
+  end
+
+  def delete
+
+  end
+
+  def find
+
+  end
+
+  def level_order
+
+  end
+
+  def inorder
+
+  end
+
+  def preorder
+
+  end
+
+  def postorder
+
+  end
+
+  def height
+
+  end
+
+  def depth
+
+  end
+
+  def balanced?
+
+  end
+
+  def rebalance
+
+  end
+
+end
